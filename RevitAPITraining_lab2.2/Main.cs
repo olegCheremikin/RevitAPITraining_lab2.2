7643bb70459bf16c2a4b53f9dee80a863f20a451
O.Cheremikin <O.Cheremikin@CZC724BBKX>
2022-02-15 15:44:42 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITraining_lab2.2/Main.cs
@@ -0,0 +1,31 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITraining_lab2._2
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var pipes = new FilteredElementCollector(doc, doc.ActiveView.Id)
+                        .OfClass(typeof(Pipe))
+                        .Cast<Pipe>()
+                        .ToList();
+
+            TaskDialog.Show("Количество труб", pipes.Count.ToString());
+            return Result.Succeeded;
+        }
+    }
+}
